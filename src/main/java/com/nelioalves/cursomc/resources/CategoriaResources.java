a1666b2b8c64ea89a1f56503ca1b36141e34de1b
Alexandre Marques Teixeira <alexandre.marques.teixeira@gmail.com>
2019-04-04 19:27:21 -0300

    Criando Repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -2,6 +2,7 @@ package com.nelioalves.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,9 @@ public class CategoriaResources {
 	@Autowired
 	private CategoriaService service;
 	
-	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(Integer id) {
-		Categoria obj = service.find(id);
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-	}
+	}	
 }
