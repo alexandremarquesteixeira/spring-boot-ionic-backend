1c5be3b671eb757dd63cbec1f3b6adfc4b79f85d
Alexandre Marques Teixeira <alexandre.marques.teixeira@gmail.com>
2019-06-02 12:28:49 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -134,13 +134,13 @@ public class DBService {
 		
 		Endereco e1 = new Endereco(null, "Rua das Flores", "300", "apto 303", "jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "centro", "38777012", cli1, c2);
-		Endereco e3 = new Endereco(null, "Avenida Floriano", "2105", null , "centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2105", null , "centro", "38777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
 		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2, e3));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
